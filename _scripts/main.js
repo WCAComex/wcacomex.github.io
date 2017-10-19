ea718172de32cfb141a571c32b1902087c9418d6
johnylab <joaobatistacomunic@hotmail.com>
2017-10-19 00:36:56 -0200

    Setup completo

---
+++ _scripts/main.js
@@ -0,0 +1,6 @@
+
+function ok() {
+    return true;
+}
+
+ok();
---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var cp          = require('child_process');
+var rollup      = require('gulp-rollup');
+var sourcemaps  = require('gulp-sourcemaps');
+
+var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+var messages = {
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
+
+gulp.task('jekyll-build', function (done) {
+    browserSync.notify(messages.jekyllBuild);
+    return cp.spawn(jekyll, ['build'], {stdio: 'inherit'})
+        .on('close', done);
+});
+
+gulp.task('bundle', function() {
+    gulp.src('./_scripts/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(rollup({
+            input: './_scripts/main.js',
+            format: 'iife'
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./js'));
+  });
+
+gulp.task('jekyll-rebuild', ['bundle', 'jekyll-build'], function () {
+    browserSync.reload();
+});
+
+gulp.task('browser-sync', ['bundle', 'jekyll-build'], function() {
+    browserSync({
+        server: {
+            baseDir: '_site'
+        }
+    });
+});
+
+gulp.task('watch', function () {
+    gulp.watch([
+        './*', 
+        '_layouts/*', 
+        '_includes/*', 
+        '_sass/*', 
+        'css/*', 
+        '_scripts/**/*', 
+        'js/*'
+    ], ['jekyll-rebuild']);
+});
+
+gulp.task('default', ['browser-sync', 'watch']);
