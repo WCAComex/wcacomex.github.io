f901c4e6262fb4903b428aa48ac17445fc872541
johnylab <joaobatistacomunic@hotmail.com>
2017-11-18 15:33:10 -0200

    Mapa com 100% e espaçamento no código

--- _scripts/libs/parallax.js
+++ _scripts/libs/parallax.js
@@ -1,18 +1,24 @@
 export default function(parent, fx) {
+
     if (typeof parent === 'string') parent = document.querySelector(parent);
     if (!parent) return console.log('Parallax container not found');
+
     const boundings = { top: parent.offsetTop - window.innerHeight, bottom: parent.offsetTop + parent.offsetHeight };
+
     for (const item in fx) {
         const selector = fx[item].element;
         if (typeof selector === 'string') fx[item].element = parent.querySelector(selector);
         if (!fx[item].element) console.log(`Child "${selector}" not found!`);
     };
+
     window.addEventListener('scroll', function(){
         if (scrollY > boundings.top && scrollY < boundings.bottom) for (const item in fx) translate(fx[item]);
     });
+
     function translate(effect) {
         if (!effect) return false;
         const translation = (scrollY - parent.offsetTop) / effect.level;
         effect.element.style.transform = `translateY(${translation}px)`;
     }
+
 };
--- _scripts/main.js
+++ _scripts/main.js
@@ -1,6 +1,9 @@
+console.time('Execução do script');
+
 import parallax from './libs/parallax';
 
 const parallaxContainer = document.querySelector('.section-features');
+
 parallaxContainer.style.overflow = 'hidden';
 parallax(parallaxContainer, [{
     element: '.feature-icon', level: 2
@@ -10,16 +13,21 @@ parallax(parallaxContainer, [{
 
 const mapContainer = document.querySelector('#map-container');
 window.addEventListener('scroll', function scrollToMap() {
-    if (scrollY + innerHeight >= mapContainer.offsetTop) {
-        const mapIframe = document.createElement('iframe');
-        mapIframe.src = mapContainer.getAttribute('data-iframe');
-        mapIframe.width = mapContainer.getAttribute('data-width');
-        mapIframe.height = mapContainer.getAttribute('data-height');
-        mapIframe.frameborder = "0";
-        mapIframe.style.border = "0";
-        mapIframe.setAttribute('allowfullscreen', true);
-        mapIframe.style.backgroundColor = '#eee';
-        mapContainer.appendChild(mapIframe);
-        window.removeEventListener('scroll', scrollToMap);
-    }
+    
+    if (scrollY + innerHeight < mapContainer.offsetTop) return false;
+    
+    const mapIframe = document.createElement('iframe');
+    mapIframe.src = mapContainer.getAttribute('data-iframe');
+    mapIframe.width = mapContainer.getAttribute('data-width');
+    mapIframe.height = mapContainer.getAttribute('data-height');
+    mapIframe.frameborder = "0";
+    mapIframe.style.border = "0";
+    mapIframe.setAttribute('allowfullscreen', true);
+    mapIframe.style.backgroundColor = '#eee';
+    mapContainer.appendChild(mapIframe);
+    
+    window.removeEventListener('scroll', scrollToMap);
+    
 });
+
+console.timeEnd('Execução do script');
