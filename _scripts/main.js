8deac494da002277c8a1d586b209bfdbed3a2371
Johnylab <joaobatistacomunic@hotmail.com>
2017-11-26 16:07:23 -0200

    Correção de pequenos erros de script

--- _scripts/main.js
+++ _scripts/main.js
@@ -2,39 +2,39 @@ console.time('Execução do script');
 
 import htmlComp from './libs/htmlComp';
 
-const siteHeader = document.querySelector('.site-header');
 const parallaxContainer = document.querySelector('.parallax-container');
-const parallaxContent = parallaxContainer.querySelector('.parallax-content');
 const mapContainer = document.querySelector('#map-container');
 
 document.body.addEventListener('touchstart', () => {});
 
-if (parallaxContainer && parallaxContent) window.addEventListener('scroll', function() {
-    if (scrollY > parallaxContainer.offsetTop + parallaxContainer.offsetHeight) return false;
-    parallaxContent.style.transform = `translateY(${scrollY / 3}px)`;
-    siteHeader.style.transform = `translateY(${scrollY / 2}px)`;
-});
+if (parallaxContainer) {
+    const siteHeader = document.querySelector('.site-header');
+    const parallaxContent = parallaxContainer.querySelector('.parallax-content');
+    if (parallaxContent) window.addEventListener('scroll', function() {
+        if (scrollY > parallaxContainer.offsetTop + parallaxContainer.offsetHeight) return false;
+        parallaxContent.style.transform = `translateY(${scrollY / 3}px)`;
+        siteHeader.style.transform = `translateY(${scrollY / 2}px)`;
+    });
+};
 
-window.addEventListener('scroll', function scrollToMap() {
-    
-    if (scrollY + innerHeight < mapContainer.offsetTop) return false;
-
-    mapContainer.appendChild(htmlComp('iframe', {
-        src: mapContainer.getAttribute('data-iframe'),
-        width: mapContainer.getAttribute('data-width'),
-        height: mapContainer.getAttribute('data-height'),
-        frameborder: "0",
-        allowfullscreen: true,
-        style: {
-            display: 'block',
-            height: '100vh',
-            border: '0',
-            backgroundColor: '#eee'
-        }
-    }));
-    
-    window.removeEventListener('scroll', scrollToMap);
-    
-});
+if (mapContainer) {
+    window.addEventListener('scroll', function scrollToMap() {
+        if (scrollY + innerHeight < mapContainer.offsetTop) return false;
+        mapContainer.appendChild(htmlComp('iframe', {
+            src: mapContainer.getAttribute('data-iframe'),
+            width: mapContainer.getAttribute('data-width'),
+            height: mapContainer.getAttribute('data-height'),
+            frameborder: "0",
+            allowfullscreen: true,
+            style: {
+                display: 'block',
+                height: '100vh',
+                border: '0',
+                backgroundColor: '#888'
+            }
+        }));
+        window.removeEventListener('scroll', scrollToMap);
+    });
+};
 
 console.timeEnd('Execução do script');
